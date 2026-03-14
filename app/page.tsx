fb7a90d1038b179e15e3e1b3c9a7e1b926f7ce4c
Andrew Pimenta <167480888+AndrewLPimenta@users.noreply.github.com>
2026-03-14 09:54:08 -0300

    teste

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,10 @@ const home = () => {
     <div>
       <h1>NUCLEOS</h1>
       <p>joao pedro conectado.</p>
-<<<<<<< HEAD
       <p>PC 1</p>
-=======
       <p>Caue Conectado</p>
->>>>>>> 8e967248977525108cfa4b159d6e2a53122dcd5a
     </div>
   );
 }
 
-export default home
+export default home;
