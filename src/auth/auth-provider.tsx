dbff0848760edf29ecb4e76790f3008c74d73fb0
Kevynn Machado Alves <kevynnmachado@hotmail.com>
2026-06-29 18:26:07 -0300

    Remove hydration comment in AuthProvider
    Removed comment regarding hydration issues with Next.js and localStorage.

--- src/auth/auth-provider.tsx
+++ src/auth/auth-provider.tsx
@@ -4,7 +4,6 @@ import { AuthProvider as InternalAuthProvider } from "./auth-context";
 import { type ReactNode, useEffect, useState } from "react";
 
 export function AuthProvider({ children }: { children: ReactNode }) {
-  // Evita problemas de hydration (Next.js + localStorage)
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
